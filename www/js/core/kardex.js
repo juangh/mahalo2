261a452d2bcf323567d817bb66f12de21e1ab9f1
juangh <juan.gomez@websaas.net.co>
2014-04-08 16:52:47 -0500

    08/04/2014
    Avance en informes de kardex

--- www/js/core/kardex.js
+++ www/js/core/kardex.js
@@ -439,3 +439,150 @@ function llenarTablaKardexClientes()
         document.getElementById("tab-kardexclientes-1").innerHTML = "<thead style='background: white; color:red;'>" + "<tr>" + "<th style='padding:15px'>La búsqueda no arroja datos</th>" + "</tr>" + "</thead>" + "<tbody style='text-align: center'></tbody>";
     }
 }
+
+/******************/
+
+function graficarHistogramaKardexTres()
+{
+    var GRAFICA = 'historial_kardex_tres';
+    var posicion = 0;
+    jQuery("canvas.peity").last().removeClass("barsKardexTres").removeClass("pointsKardexTres");
+    jQuery("#histogramaKardexTres").css("background", "#78bde7");
+    jQuery("#tortaKardexTres").css("background", "#2f2f2f");
+    jQuery("#puntosKardexTres").css("background", "#2f2f2f");
+    jQuery.fn.peity.defaults.bar = {colours: ["#4d89f9"], delimiter: ",", height: "100%", max: null, min: 0, spacing: 1, width: "100%"};
+    var puntos = traerTop10KardexTres();
+    jdg = puntos;
+    if (puntos.size !== -1) {
+        if (puntos) {
+            var barras = "";
+            for (var i = 0; i < puntos.size + 1; i++) {
+                barras = barras + "," + puntos[i]['y'];
+            }
+            if (barras === '') {
+                $("#contenidoGraficaTres").html("<div style='background: white; margin-left: 25px; margin-right:25px; border:1px solid #CCC; color:red;'><h4 style='text-align:center; color:red;'>No hay datos para realizar la graficación...</h4></div>");
+            } else {
+                jQuery("#contenidoGraficaKardexTres").html('');
+                jQuery("#contenidoGraficaKardexTres").html('<div id="barKardexTres" style="display:none;"></div><div id="pieKardexTres" style="display:none;"></div><div id="lineKardexTres" style="display:none;"></div>');
+                jQuery("#barKardexTres").text(barras.substr(1));
+                jQuery("#barKardexTres").peity("bar");
+                jQuery("#contenidoGraficaTres").append($("canvas.peity"));
+                for (var i = 0; i < $("canvas.peity").length; i++) {
+                    if ($("canvas.peity")[i].id === GRAFICA) {
+                        posicion = i;
+                    }
+                }
+                if (inicioHistogramaKardexTres) {
+                    $current = $("canvas.peity:nth-last-child(" + posicion + ")");
+                    inicioHistogramaKardexTres = false;
+                }
+            }
+        }
+    }
+}
+function graficarTortaKardexTres()
+{
+    var GRAFICA = 'torta_kardex_tres';
+    var posicion = 0;
+    jQuery("canvas.peity").removeClass("histKardexTres").removeClass("pointsKardexTres");
+    jQuery("#histogramaKardexTres").css("background", "#2f2f2f");
+    jQuery("#tortaKardexTres").css("background", "#78bde7");
+    jQuery("#puntosKardexTres").css("background", "#2f2f2f");
+    jQuery.fn.peity.defaults.pie = {colours: ["#ff9900", "#fff4dd", "#ffd592"], delimiter: null, diameter: "100%", height: null, width: null};
+    var puntos = traerTop10KardexTres();
+    if (puntos.size !== -1) {
+        if (puntos) {
+            var barras = "";
+            for (var i = 0; i < puntos.size + 1; i++) {
+                barras = barras + "," + puntos[i]['y'];
+            } 
+            jQuery("#contenidoGraficaKardexTres").html('');
+            jQuery("#contenidoGraficaKardexTres").html('<div id="barKardexTres" style="display:none;"></div><div id="pieKardexTres" style="display:none;"></div><div id="lineKardexTres" style="display:none;"></div>');
+            jQuery("#pieKardexKardexTres").text(barras.substr(1));
+            jQuery("#pieKardexKardexTres").peity("pie");
+            jQuery("#contenidoGraficaKardexTres").append($("canvas.peity"));
+            for (var i = 0; i < $("canvas.peity").length; i++) {
+                if ($("canvas.peity")[i].id === GRAFICA) {
+                    posicion = i;
+                }
+            }
+            if (inicioTortaKardexTres) {
+                $current = $("canvas.peity:nth-last-child(" + posicion + ")");
+                inicioTortaKardexTres = false;
+            }
+        }
+    }
+}
+function graficarLineaKardexTres()
+{
+    var GRAFICA = 'linea_kardex_tres';
+    var posicion = 0;
+    jQuery("canvas.peity").removeClass("histKardexTres").removeClass("barsKardexTres");
+    jQuery("#histogramaKardexTres").css("background", "#2f2f2f");
+    jQuery("#tortaKardexTres").css("background", "#2f2f2f");
+    jQuery("#puntosKardexTres").css("background", "#78bde7");
+    jQuery.fn.peity.defaults.line = {colour: "#c6d9fd", strokeColour: "#4d89f9", strokeWidth: 1, delimiter: ",", height: "100%", max: null, min: 0, width: "100%"};
+    var puntos = traerTop10KardexTres();
+    if (puntos.size !== -1) {
+        if (puntos) {
+            var barras = "";
+            for (var i = 0; i < puntos.size + 1; i++) {
+                barras = barras + "," + puntos[i]['y'];
+            }
+            jQuery("#contenidoGraficaKardexTres").html('');
+            jQuery("#contenidoGraficaKardexTres").html('<div id="barKardexTres" style="display:none;"></div><div id="pieKardexTres" style="display:none;"></div><div id="lineKardexTres" style="display:none;"></div>');
+            jQuery("#lineKardexKardexTres").text(barras.substr(1));
+            jQuery("#lineKardexTres").peity("line");
+            jQuery("#contenidoGraficaKardexTres").append(jQuery("canvas.peity"));
+            for (var i = 0; i < $("canvas.peity").length; i++) {
+                if ($("canvas.peity")[i].id === GRAFICA) {
+                    posicion = i; 
+                }
+            }
+            if (inicioLineaKardexTres) {
+                $current = $("canvas.peity:nth-last-child(" + posicion + ")");
+                inicioKardexTres = false;
+            }
+        }
+    }
+}
+
+function traerTop10KardexTres()
+{
+    var date = formatearFecha(new Date().toString());
+    var id_query = "busqueda_top_kardex_tres";
+    var sql = "select h.cc_cliente cedula, sum(cn_kardex) cantidad from h_kardex as h inner join mv_kardex as m on h.c_concepto_mov = m.c_concepto_mov and m.f_kardex > '"+date+"'  group by h.cc_cliente order by 2 desc";
+    xmlQueryDB(sql, id_query, 1, false, ruta);
+    var ar_status = getStatusDB(id_query);
+    var size = ar_status['numrows'] - 1;
+    var ar_nits = [], ar_valores = [];
+    var puntos = {};
+    for (var u = 0; u <= size; u++) {
+        ar_nits[u] = xmlGetRow(id_query, u + 1, 0)['cedula'];
+        ar_valores[u] = xmlGetRow(id_query, u + 1, 0)['cantidad'];
+        puntos[u] = {x: ar_nits[u], y: ar_valores[u]};
+    }
+    puntos.size = size;
+    return puntos;
+}
+
+function llenarTablaKardexTres()
+{
+    var datos = traerTop10KardexTres();
+    var size = datos.size + 1;
+    var filas = [];
+    for (var i = 0; i < size; i++) {
+        document.getElementById("tab-kardextres-1").innerHTML = "";
+    }
+    document.getElementById("tab-kardextres-1").innerHTML = "<thead style='background: #d0e841'>" + "<tr>" + "<th align='left'>&nbsp;&nbsp;<span data-lang-id='lang137'>Registro</span></th>" + "<th align='left'>&nbsp;&nbsp;<span data-lang-id=''>Campo1</span></th>" + "<th align='right'><span data-lang-id=''>Campo2</span>&nbsp;&nbsp;</th>" + "</tr>" + "</thead>" + "<tbody style='text-align: center'></tbody>";
+    for (var i = 0; i < size; i++) {
+        filas[i] = document.createElement("tr");
+        filas[i].setAttribute("id", "tr_" + i);
+        filas[i].setAttribute("style", "text-align:center");
+        filas[i].innerHTML = "<td align='left'>" + espacio + (i + 1) + "</td><td align='left'>" + espacio + datos[i]['x'] + "</td>" + '<td align="right">' + " $" + toCurrency(datos[i]['y']) + "" + espacio + "</td>";
+        document.getElementById("tab-kardextres-1").appendChild(filas[i]);
+    }
+    if (size === 0) {
+        document.getElementById("tab-kardextres-1").innerHTML = "<thead style='background: white; color:red;'>" + "<tr>" + "<th style='padding:15px'>La búsqueda no arroja datos</th>" + "</tr>" + "</thead>" + "<tbody style='text-align: center'></tbody>";
+    }
+}
